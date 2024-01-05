3406d0991fedcdee622ff72c16a3f18ca2f9f3fd
Amit Dhingra <94193086+Amit-Dhingra180@users.noreply.github.com>
2024-01-05 18:47:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+from hello_logic import say_hello_logic  # Import the logic function
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/say-hello', methods=['POST'])
+def say_hello():
+    input_data = request.json
+    user_input = input_data.get('input', '')
+
+    output = say_hello_logic(user_input)
+
+    result = {'output': output}
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
